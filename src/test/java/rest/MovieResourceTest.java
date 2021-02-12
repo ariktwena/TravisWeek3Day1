be52056b4807525a753943be984df2474ad8eae7
ariktwena <ariktwena@gmail.com>
2021-02-12 02:03:36 +0100

    1 test error

--- src/test/java/rest/MovieResourceTest.java
+++ src/test/java/rest/MovieResourceTest.java
@@ -83,24 +83,24 @@ public class MovieResourceTest {
         given().when().get("/movie/all").then().statusCode(200);
     }
 
-    //This test assumes the database contains two rows
+//    //This test assumes the database contains two rows
 //    @Test
-    public void testDummyMsg() throws Exception {
-        given()
-                .contentType("application/json")
-                .get("/xxx/").then()
-                .assertThat()
-                .statusCode(HttpStatus.OK_200.getStatusCode())
-                .body("msg", equalTo("Hello World"));
-    }
-
+//    public void testDummyMsg() throws Exception {
+//        given()
+//                .contentType("application/json")
+//                .get("/xxx/").then()
+//                .assertThat()
+//                .statusCode(HttpStatus.OK_200.getStatusCode())
+//                .body("msg", equalTo("Hello World"));
+//    }
+//
 //    @Test
-    public void testCount() throws Exception {
-        given()
-                .contentType("application/json")
-                .get("/xxx/count").then()
-                .assertThat()
-                .statusCode(HttpStatus.OK_200.getStatusCode())
-                .body("count", equalTo(2));
-    }
+//    public void testCount() throws Exception {
+//        given()
+//                .contentType("application/json")
+//                .get("/xxx/count").then()
+//                .assertThat()
+//                .statusCode(HttpStatus.OK_200.getStatusCode())
+//                .body("count", equalTo(2));
+//    }
 }
